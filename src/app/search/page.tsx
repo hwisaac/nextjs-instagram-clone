66093546d1c066a4fac48fef82470593a06a3475
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-04-27 17:19:27 +0900

    페이지/라우트: static -> dynamic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,8 @@ import UserSearch from '@/components/UserSearch';
 import { Metadata } from 'next';
 import React from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'User Search',
   description: 'Search users to follow',
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,14 +2,15 @@ import UserProfile from '@/components/UserProfile';
 import UserPosts from '@/components/UserPosts';
 import { getUserForProfile } from '@/service/user';
 import { notFound } from 'next/navigation';
-import React from 'react';
+import React, { cache } from 'react';
 import { Metadata } from 'next';
-
 
 type Props = { params: { username: string } };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function page({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
   if (!user) {
     notFound();
   }
@@ -25,9 +26,10 @@ export default async function page({ params: { username } }: Props) {
 export async function generateMetadata({
   params: { username },
 }: Props): Promise<Metadata> {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   return {
     title: `${user?.name} (@${user?.username}) • Instantgram Photos`,
+    description: `${user?.name}'s all Instantgram posts`,
   };
 }
