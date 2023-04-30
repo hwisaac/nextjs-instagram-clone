f960d113895a9fdd2c0638d030038cabf859d7b6
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-04-30 16:15:18 +0900

    useCallback 을 이용한 성능개선

--- src/hooks/me.ts
+++ src/hooks/me.ts
@@ -1,5 +1,6 @@
 import { SimplePost } from '@/model/post';
 import { HomeUser } from '@/model/user';
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 async function updateBookmark(postId: string, bookmark: boolean) {
@@ -12,23 +13,26 @@ async function updateBookmark(postId: string, bookmark: boolean) {
 export default function useMe() {
   const { data: user, isLoading, error, mutate } = useSWR<HomeUser>('/api/me');
 
-  const setBookmark = (postId: string, bookmark: boolean) => {
-    if (!user) return;
+  const setBookmark = useCallback(
+    (postId: string, bookmark: boolean) => {
+      if (!user) return;
 
-    const bookmarks = user.bookmarks;
-    const newUser = {
-      ...user,
-      bookmarks: bookmark
-        ? [...bookmarks, postId]
-        : bookmarks.filter((b) => b !== postId),
-    };
+      const bookmarks = user.bookmarks;
+      const newUser = {
+        ...user,
+        bookmarks: bookmark
+          ? [...bookmarks, postId]
+          : bookmarks.filter((b) => b !== postId),
+      };
 
-    return mutate(updateBookmark(postId, bookmark), {
-      optimisticData: newUser,
-      populateCache: false, // 업데이트할 데이터값은 updateLike의 반환값 말고 optimisticData 로 그대로 업데이트
-      revalidate: false,
-      rollbackOnError: true, // 로컬에서 캐시를 업댓헀는데, 네트워크 통신에서 문제가 발생하면 원래대로 롤백
-    });
-  };
+      return mutate(updateBookmark(postId, bookmark), {
+        optimisticData: newUser,
+        populateCache: false, // 업데이트할 데이터값은 updateLike의 반환값 말고 optimisticData 로 그대로 업데이트
+        revalidate: false,
+        rollbackOnError: true, // 로컬에서 캐시를 업댓헀는데, 네트워크 통신에서 문제가 발생하면 원래대로 롤백
+      });
+    },
+    [user, mutate]
+  );
   return { user, isLoading, error, setBookmark };
 }
--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,4 +1,5 @@
 import { Comment, SimplePost } from '@/model/post';
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 async function updateLike(id: string, like: boolean) {
@@ -23,37 +24,43 @@ export default function usePosts() {
     mutate,
   } = useSWR<SimplePost[]>('/api/posts');
 
-  const setLike = (post: SimplePost, username: string, like: boolean) => {
-    const newPost = {
-      ...post,
-      likes: like
-        ? [...post.likes, username]
-        : post.likes.filter((item) => item !== username),
-    };
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+  const setLike = useCallback(
+    (post: SimplePost, username: string, like: boolean) => {
+      const newPost = {
+        ...post,
+        likes: like
+          ? [...post.likes, username]
+          : post.likes.filter((item) => item !== username),
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
 
-    return mutate(updateLike(post.id, like), {
-      optimisticData: newPosts,
-      populateCache: false, // 업데이트할 데이터값은 updateLike의 반환값 말고 optimisticData 로 그대로 업데이트
-      revalidate: false,
-      rollbackOnError: true, // 로컬에서 캐시를 업댓헀는데, 네트워크 통신에서 문제가 발생하면 원래대로 롤백
-    });
-  };
+      return mutate(updateLike(post.id, like), {
+        optimisticData: newPosts,
+        populateCache: false, // 업데이트할 데이터값은 updateLike의 반환값 말고 optimisticData 로 그대로 업데이트
+        revalidate: false,
+        rollbackOnError: true, // 로컬에서 캐시를 업댓헀는데, 네트워크 통신에서 문제가 발생하면 원래대로 롤백
+      });
+    },
+    [posts, mutate]
+  );
 
-  const postComment = (post: SimplePost, comment: Comment) => {
-    const newPost = {
-      ...post,
-      comments: post.comments + 1,
-    };
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+  const postComment = useCallback(
+    (post: SimplePost, comment: Comment) => {
+      const newPost = {
+        ...post,
+        comments: post.comments + 1,
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPosts,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate]
+  );
 
   return { posts, isLoading, error, setLike, postComment };
 }
